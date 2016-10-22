f49730f6fa308c1948bae19932a7bf010b8cb4cd
JaroslawWiosna <JaroslawWiosna@users.noreply.github.com>
2016-10-23 01:00:41 +0200

    Statistics <- (mean, median, var, stddev) (#10)
    * Statistics <- (mean, median, var, stddev)
    
    * eps changed to constexpr

--- tests/correctTests.cpp
+++ tests/correctTests.cpp
@@ -684,6 +684,29 @@ TEST_F(MatrixTest, getminor)
   ASSERT_TRUE(compare(getminor(C,1,1), minor1x1, 0.01f));
 }
 
+TEST_F(MatrixTest, stats)
+{
+  constexpr float eps = 0.01f;
+
+  const Matrix<float, 3, 3> A =
+  { 1, 2, 3,
+    4, 5, 6,
+    7, 8, 9 };
+  ASSERT_TRUE((mean(A)   - 5)             < eps);
+  ASSERT_TRUE((median(A) - 5)             < eps);
+  ASSERT_TRUE((var(A)    - 6.66667)       < eps);
+  ASSERT_TRUE((stddev(A) - sqrt(6.66667)) < eps);
+
+  const Matrix<float, 3, 3> B =
+  { 2, 2, 2,
+    5, 5, 5,
+    8, 8, 8 };
+  ASSERT_TRUE((mean(B)   - 5)       < eps);
+  ASSERT_TRUE((median(B) - 5)       < eps);
+  ASSERT_TRUE((var(B)    - 6)       < eps);
+  ASSERT_TRUE((stddev(B) - sqrt(6)) < eps);
+}
+
 int main(int argc, char* argv[])
 {
   ::testing::InitGoogleTest(&argc, argv);
