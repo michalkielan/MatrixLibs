8a09e42122b7dea526cd6dee0816c8a9bedd9aad
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2016-10-28 13:31:30 +0200

    Conv2 enhancement that makes reurn matrix equal to zero.

--- tst/correct_test/correctTests.cpp
+++ tst/correct_test/correctTests.cpp
@@ -563,16 +563,31 @@ TEST_F(MatrixTest, convolution)
    1/9, 1/9, 1/9,
    1/9, 1/9, 1/9 };
 
-  const Matrix<float, 5, 5> expected_after_conv =
- { 0,  0,  0,  0, 0,
-   0, 12, 13, 14, 0,
-   0, 12, 13, 14, 0,
-   0,  9,  9,  9, 0,
-   0,  0,  0,  0, 0};
+  const Matrix<float, 7, 7> conv_full =
+ { 1, 2,  3,  3,  3,  2, 1,
+   3, 6,  9, 10, 11,  8, 4,
+   4, 8, 12, 13, 14, 10, 5,
+   4, 8, 12, 13, 14, 10, 5,
+   3, 6,  9,  9,  9,  6, 3,
+   2, 4,  6,  6,  6,  4, 2, 
+   1, 2,  3,  3,  3,  2, 1 };
 
-  auto after_conv = conv2(A,kernel);
+  const Matrix<float, 5, 5> conv_same =
+ { 6,  9, 10, 11,  8,
+   8, 12, 13, 14, 10,
+   8, 12, 13, 14, 10,
+   6,  9,  9,  9,  6,
+   4,  6,  6,  6,  4 };
 
-  ASSERT_TRUE(compare(expected_after_conv, after_conv, 0.0001f));
+  const Matrix<float, 3, 3> conv_valid =
+ { 12, 13, 14,
+   12, 13, 14,
+    9,  9,  9, }; 
+  //print(conv2(A,kernel));
+  ASSERT_TRUE(compare(conv_same, conv2(A,kernel), 0.0001f));
+  // The following two lines are not implemented
+  // ASSERT_TRUE(compare(conv_full, conv2_full(A,kernel), 0.0001f));
+  // ASSERT_TRUE(compare(conv_valid, conv2_valid(A,kernel), 0.0001f));
 
   const Matrix<float, 5, 5> B =
  { 5, 5, 5, 5, 5,
