1a1a7cd5f8ade4830d9a24d072f948972c1281f8
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2016-10-29 20:57:28 +0200

    Convolution implemented (3 options)

--- matrix/matrix_conv2.hpp
+++ matrix/matrix_conv2.hpp
@@ -9,6 +9,7 @@
 #define MATRIX_MATRIX_CONV2_HPP_
 
 #include "matrix_type.hpp"
+#include "matrix_inv.hpp"  //getminor needed TODO move getminor to i.e. utils?
 
 namespace mlib
 {
@@ -31,27 +32,111 @@ template<typename T, std::size_t i, std::size_t j>
 Matrix<T, i, j> conv2(const Matrix<T, i, j>& A, const Matrix<T, 3, 3>& kernel)
 {
   Matrix<T, i, j> result{};
-
-  for(std::size_t it = 1; it < i-1; it++)
+  auto isOutOfRange = [](int a, int b)
   {
-    for(std::size_t jt = 1; jt < j-1; jt++)
+    std::size_t result = 0;
+    if (a < 0)
     {
-      result[it][jt] = kernel[0][0] * A[it-1][jt-1] +
-      kernel[0][1] * A[it-1][jt] +
-      kernel[0][2] * A[it-1][jt+1] +
-      kernel[1][0] * A[it][jt-1] +
-      kernel[1][1] * A[it][jt] +
-      kernel[1][2] * A[it][jt+1] +
-      kernel[2][0] * A[it+1][jt-1] +
-      kernel[2][1] * A[it+1][jt] +
-      kernel[2][2] * A[it+1][jt+1];
+      result++;
+    }
+    if (b < 0)
+    {
+      result++;
+    }
+    if (static_cast<unsigned int>(a) >= i)
+    {
+      result++;
+    }
+    if (static_cast<unsigned int>(b) >= j)
+    {
+      result++;
+    }
+    return result;
+  };
+
+  for(std::size_t it = 0; it < i; it++)
+  {
+    for(std::size_t jt = 0; jt < j; jt++)
+    {
+      result[it][jt] = 0;
+      result[it][jt] += ((isOutOfRange(it-1,jt-1)) ? 0 : kernel[0][0] * A[it-1][jt-1]);   
+      result[it][jt] += ((isOutOfRange(it-1,jt))   ? 0 : kernel[0][1] * A[it-1][jt]);
+      result[it][jt] += ((isOutOfRange(it-1,jt+1)) ? 0 : kernel[0][2] * A[it-1][jt+1]);
+      result[it][jt] += ((isOutOfRange(it,jt-1))   ? 0 : kernel[1][0] * A[it][jt-1]);   
+      result[it][jt] += ((isOutOfRange(it,jt))     ? 0 : kernel[1][1] * A[it][jt]);   
+      result[it][jt] += ((isOutOfRange(it,jt+1))   ? 0 : kernel[1][2] * A[it][jt+1]);   
+      result[it][jt] += ((isOutOfRange(it+1,jt-1)) ? 0 : kernel[2][0] * A[it+1][jt-1]);  
+      result[it][jt] += ((isOutOfRange(it+1,jt))   ? 0 : kernel[2][1] * A[it+1][jt]);
+      result[it][jt] += ((isOutOfRange(it+1,jt+1)) ? 0 : kernel[2][2] * A[it+1][jt+1]); 
     }
   }
-
   return result;
 }
 
-} /* namespace mlib */
+//experimental conv2 with 'full' option
+//returns i+2 x j+2 because kernel is 3x3
+//basic rule is i+(n-1) x j+(n-1)
+// TODO move in inside conv2() (add parameter 'valid' as option)
+// TODO refactor it so it will look more friendly
+template<typename T, std::size_t i, std::size_t j>
+Matrix<T, i+2, j+2> conv2_full(const Matrix<T, i, j>& A, const Matrix<T, 3, 3>& kernel)
+{
+  Matrix<T, i+2, j+2> result{};
+  // equals 1 because kernel is 3x3, if kernel is 5x5 offset would be 2 
+  constexpr std::size_t offset{1};
+  auto isOutOfRange = [](int a, int b)
+  {
+    std::size_t result = 0;
+    if (a < 0)
+    {
+      result++;
+    }
+    if (b < 0)
+    {
+      result++;
+    }
+    if (static_cast<unsigned int>(a) >= i+2)
+    {
+      result++;
+    }
+    if (static_cast<unsigned int>(b) >= j+2)
+    {
+      result++;
+    }
+    return result;
+  };
+
+  for(std::size_t it = 0; it < i+2; it++)
+  {
+    for(std::size_t jt = 0; jt < j+2; jt++)
+    {
+      result[it][jt] = 0;
+      result[it][jt] += ((isOutOfRange(it-1 - offset,jt-1 - offset)) ? 0 : kernel[0][0] * A[it-1 - offset][jt-1 - offset]);   
+      result[it][jt] += ((isOutOfRange(it-1 - offset,jt - offset))   ? 0 : kernel[0][1] * A[it-1 - offset][jt - offset]);
+      result[it][jt] += ((isOutOfRange(it-1 - offset,jt+1 - offset)) ? 0 : kernel[0][2] * A[it-1 - offset][jt+1 - offset]);
+      result[it][jt] += ((isOutOfRange(it - offset,jt-1 - offset))   ? 0 : kernel[1][0] * A[it - offset][jt-1 - offset]);   
+      result[it][jt] += ((isOutOfRange(it - offset,jt - offset))     ? 0 : kernel[1][1] * A[it - offset][jt - offset]);   
+      result[it][jt] += ((isOutOfRange(it - offset,jt+1 - offset))   ? 0 : kernel[1][2] * A[it - offset][jt+1 - offset]);   
+      result[it][jt] += ((isOutOfRange(it+1 - offset,jt-1 - offset)) ? 0 : kernel[2][0] * A[it+1 - offset][jt-1 - offset]);  
+      result[it][jt] += ((isOutOfRange(it+1 - offset,jt - offset))   ? 0 : kernel[2][1] * A[it+1 - offset][jt - offset]);
+      result[it][jt] += ((isOutOfRange(it+1 - offset,jt+1 - offset)) ? 0 : kernel[2][2] * A[it+1 - offset][jt+1 - offset]); 
+    }
+  }
+  return result;
+}
+
+//experimental conv2 with 'valid' option
+//returns i-2 x j-2 because kernel is 3x3
+//basic rule is i-(n-1) x j-(n-1)
+// TODO move in inside conv2() (add parameter 'valid' as option)
+template<typename T, std::size_t i, std::size_t j>
+Matrix<T, i-2, j-2> conv2_valid(const Matrix<T, i, j>& A, const Matrix<T, 3, 3>& kernel)
+{
+  auto tmp1 = getminor(conv2(A,kernel),i,j); // conv2 means conv2 with -same option
+  return getminor(tmp1,0,0);
+}
 
 
+
+}
 #endif /* MATRIX_MATRIX_CONV2_HPP_ */
--- tst/conv/conv.cpp
+++ tst/conv/conv.cpp
@@ -22,21 +22,34 @@ TEST(MatrixTest, conv_1)
     9,  9,  9,  9,  9};
 
   Matrix<float, 3, 3> kernel =
-  {1/9, 1/9, 1/9,
-   1/9, 1/9, 1/9,
-   1/9, 1/9, 1/9 };
+  {0.11111111, 0.11111111, 0.11111111,
+   0.11111111, 0.11111111, 0.11111111,
+   0.11111111, 0.11111111, 0.11111111 };
 
-  const Matrix<float, 5, 5> expected_after_conv =
- { 0,  0,  0,  0, 0,
-   0, 12, 13, 14, 0,
-   0, 12, 13, 14, 0,
-   0,  9,  9,  9, 0,
-   0,  0,  0,  0, 0};
+  const Matrix<float, 7, 7> conv_full =
+ { 1, 2,  3,  3,  3,  2, 1,
+   3, 6,  9, 10, 11,  8, 4,
+   4, 8, 12, 13, 14, 10, 5,
+   4, 8, 12, 13, 14, 10, 5,
+   3, 6,  9,  9,  9,  6, 3,
+   2, 4,  6,  6,  6,  4, 2, 
+   1, 2,  3,  3,  3,  2, 1 };
 
-  auto after_conv = conv2(A,kernel);
+  const Matrix<float, 5, 5> conv_same =
+ { 6,  9, 10, 11,  8,
+   8, 12, 13, 14, 10,
+   8, 12, 13, 14, 10,
+   6,  9,  9,  9,  6,
+   4,  6,  6,  6,  4 };
 
-  ASSERT_TRUE(compare(expected_after_conv, after_conv, 0.0001f));
+  const Matrix<float, 3, 3> conv_valid =
+ { 12, 13, 14,
+   12, 13, 14,
+    9,  9,  9, }; 
 
+  ASSERT_TRUE(compare(conv_same, conv2(A,kernel), 0.1f));
+  ASSERT_TRUE(compare(conv_full, conv2_full(A,kernel), 0.0001f));
+  ASSERT_TRUE(compare(conv_valid, conv2_valid(A,kernel), 0.0001f));
 }
 
 TEST(MatrixTest, conv_highpass)
@@ -55,11 +68,11 @@ TEST(MatrixTest, conv_highpass)
    -1, -1, -1 };
 
   const Matrix<float, 5, 5> expected_after =
-  { 0, 0, 0,  0, 0,
-    0, 5, 1,  1, 0,
-    0, 5, 1, 41, 0,
-    0, 5, 1,  1, 0,
-    0, 0, 0,  0, 0};
+  { 30, 20, 20, 20, 30,
+    20,  5,  1,  1, 16,
+    20,  5,  1, 41, 16,
+    20,  5,  1,  1, 16,
+    30, 20, 20, 20, 30};
 
   auto after = conv2(A, kernel);
 
