3e69765e80b9f417c4ba9db478e8234749622192
Michal Kielan <michalkielan6@gmail.com>
2016-10-31 17:07:06 +0100

    add tests for lu

--- tst/lu/lu.cpp
+++ tst/lu/lu.cpp
@@ -19,8 +19,8 @@ TEST(MatrixTest, LU_3x3)
     3, 0, 4};
 
   auto LU = lu(A);
-  auto&& L = LU.first;
-  auto&& U = LU.second;
+  auto L = LU.first;
+  auto U = LU.second;
 
   const Matrix<float, 3, 3> expected_L =
   {     1,      0, 0,
@@ -35,13 +35,7 @@ TEST(MatrixTest, LU_3x3)
     0,     0, 16/7.0};
 
   ASSERT_TRUE(compare(expected_U, U, 0.0001f));
-
-  const Matrix<float, 3, 3> random =
-  { 1,     1,  -1000,
-    1, 7/5.0, -2/5.0,
-    1,     1,      1};
-
-  ASSERT_FALSE(compare(random, U, 0.0001f));
+  ASSERT_TRUE(compare(A, L*U, 0.0001f));
 }
 
 
@@ -66,13 +60,59 @@ TEST(MatrixTest, LU_2x2)
     0, -1.5};
 
   ASSERT_TRUE(compare(expected_U, U, 0.0001f));
+  ASSERT_TRUE(compare(A, L*U, 0.0001f));
 
-  const Matrix<float, 2, 2> random =
-   { 4.0f, 12.0f,
-     9.0f, 12.0f};
-
-  ASSERT_FALSE(compare(random, U, 0.0001f));
 }
+
+
+TEST(MatrixTest, LU_4_4_bad_matrix)
+{
+  const Matrix<float, 4, 4> A =
+  {  1,     2,     3,     4,
+     5,     6,     7,     8,
+     10,    11,    12,    13,
+     14,    15,    16,    17 };
+
+  try
+  {
+    auto LU = lu(A);
+  }
+
+  catch(std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+    ASSERT_TRUE(true);
+  }
+
+}
+
+
+TEST(MatrixTest, LU_3_3_LU_workaround)
+{
+  Matrix<float, 3, 3> A =
+  { 1, 2, 3,
+    2, 4, 5,
+    1, 3, 4 };
+
+  try
+  {
+    swap_rows(A, 1, 2);
+
+    auto LU = lu(A);
+    auto L = LU.first;
+    auto U = LU.second;
+
+    ASSERT_TRUE(compare(A, L*U, 0.0001f));
+  }
+
+  catch(std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+    ASSERT_TRUE(false);
+  }
+
+}
+
 
 
 
